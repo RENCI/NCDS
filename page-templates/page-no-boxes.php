1cb7a6da414ab2601ccac84206dd8e479a6e271d
Matt Watson <mattwat@gmail.com>
2021-04-15 12:52:25 -0400

    add ncds theme files

---
+++ page-templates/page-no-boxes.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Default No boxes
+*/
+?>
+
+<?php get_header(); ?>
+<?php get_template_part('/template-parts/interior_top'); ?>
+<section class="interior page">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-md-12 side-direction side-direction-left">
+				<?php if (have_posts()) : ?>
+					<?php while (have_posts()) : the_post(); ?>
+						<?php get_template_part('/template-parts/content_page'); ?>	
+					<?php endwhile; ?>
+				<?php endif; ?>
+			</div> 
+		</div>
+	</div> 
+</section>
+
+<?php get_footer(); ?>
